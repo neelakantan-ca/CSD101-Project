a349529ebf36f5f3f76424ae3d8f638ede990ffc
Reith <35187140+Reith77@users.noreply.github.com>
2024-11-23 13:26:15 +0530

    Added GUI Event Loop

--- src/main.c
+++ src/main.c
@@ -1,53 +1,94 @@
 #include <stdio.h>
+
 #define STRING_LENGTH 20
-int create(char name[STRING_LENGTH], char warehouse[STRING_LENGTH], char location[STRING_LENGTH], int quantity, float price) {
-    // Simulated creation function
-    return 0; // For now,returning 0 as a placeholder
-}
-int gui_user_input_print_confirm(char iname[STRING_LENGTH], char wh[STRING_LENGTH], char loc[STRING_LENGTH], int iquant, float iprice) {
-    char confirm;
-    int k;
-    //Printing of Items for user confirmation
-    printf("Item name: %s \n", iname);
-    printf("Item quantity: %d \n", iquant);
-    printf("Warehouse: %s \n", wh);
-    printf("Location: %s \n", loc);
-    printf("Price: %f \n", iprice);
-    
-    printf("Press Y to confirm, Press N to cancel: ");
-    scanf(" %c", &confirm);  
-    //If confirmed,create function gets called
-    if (confirm == 'Y' || confirm == 'y') {
-        k = create(iname, wh, loc, iquant, iprice);
-        //Confirmation message for Data Saving
-        if (k == 0) {
-            printf("Error Saving Data in File!\n");
-        } else if (k == 1) {
-            printf("Data Saved Successfully!\n");
-        }
-    } else if (confirm == 'N' || confirm == 'n') {
-        return 0;
-    } else {
-        printf("Invalid Confirmation! Please try again.\n");
-    }
-    return 1;
-}
-int main() {
-    char item_name[STRING_LENGTH], whouse[STRING_LENGTH], locat[STRING_LENGTH];
-    int item_quant;
-    float item_price;
-//user input
+
+enum gui_options {
+    LIST = 1,
+    CREATE = 2,
+    UPDATE = 3,
+    DELETE = 4,
+    SAVE = 5,
+    ACCEPT = 1,
+    REJECT = 0,
+};
+
+void gui_create() {
+
+    printf("\n\t[CREATE Entry]\n\n");
+
+    char name[STRING_LENGTH], warehouse[STRING_LENGTH], location[STRING_LENGTH];
+    int quantity;
+    float price;
+
+input:
+
     printf("Enter Item Name: ");
-    scanf("%s", item_name);  
-    printf("Enter Item Quantity: ");
-    scanf("%d", &item_quant);
+    scanf("%s", name);  
     printf("Enter Warehouse: ");
-    scanf("%s", whouse);  
+    scanf("%s", warehouse);  
     printf("Enter Location: ");
-    scanf("%s", locat);  
+    scanf("%s", location);  
+    printf("Enter Item Quantity: ");
+    scanf("%d", &quantity);
     printf("Price of Item: ");
-    scanf("%f", &item_price);
-    // Call the function to print the items and confirm if they are correct as per the user requirements
-    gui_user_input_print_confirm(item_name, whouse, locat, item_quant, item_price);
+    scanf("%f", &price);
+    
+    printf("\n\t[CONFIRM DATA]\n");
+
+    printf("Name: %s\n", name);
+    printf("Warehouse: %s\n", warehouse);
+    printf("Location: %s\n", location);
+    printf("Quantity: %d\n", quantity);
+    printf("Price: %f\n", price);
+
+    int choice;
+    printf("\nIs the data correct (1/0): ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case ACCEPT:
+            // Call function to add data to struct
+            break;
+        case REJECT:
+            goto input;
+    }
+}
+
+int main() {
+
+    printf("\n");
+
+    while (1) {
+        int choice;
+        printf("\t-[CHOICES]-\n\n");
+        printf("\t1. List inventory data\n");
+        printf("\t2. Create entry\n");
+        printf("\t3. Update entry\n");
+        printf("\t4. Delete entry\n");
+        printf("\t5. Save entries\n");
+        printf("\nEnter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case LIST:
+                printf("\n\t[SELECTED OPTION: LIST]\n\n");
+                break;
+            case CREATE:
+                printf("\n\t[SELECTED OPTION: CREATE]\n\n");
+                break;
+            case UPDATE:
+                printf("\n\t[SELECTED OPTION: UPDATE]\n\n");
+                break;
+            case DELETE:
+                printf("\n\t[SELECTED OPTION: DELETE]\n\n");
+                break;
+            case SAVE:
+                printf("\n\t[SELECTED OPTION: SAVE]\n\n");
+                break;
+            default:
+                printf("\n\t[SELECTED OPTION: INVALID CHOICE]\n\n");
+        }
+    }
+
     return 0;  
 }
