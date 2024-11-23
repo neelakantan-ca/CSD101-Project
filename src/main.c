9ef80cd1520f1b12b3bc9cd9d81d350d17eddb14
Reith <35187140+Reith77@users.noreply.github.com>
2024-11-23 17:58:44 +0530

    Added function to list data in struct

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ struct item_data {
     size_t location_size;
     int *quantity;
     size_t quantity_size;
-    float *price;
+    int *price;
     size_t price_size;
 };
 
@@ -94,7 +94,7 @@ int save_data(struct item_data *data) {
         return 0;
     }
     for (size_t i = 0; i < data->price_size; i++) {
-        if (fwrite(data->price + i, sizeof(float), 1, file) != 1) {
+        if (fwrite(data->price + i, sizeof(int), 1, file) != 1) {
             perror("Error writing price data");
             fclose(file);
             return 0;
@@ -181,9 +181,9 @@ int read_data(struct item_data *data) {
         fclose(file);
         return 0;
     }
-    data->price = calloc(data->price_size, sizeof(float));
+    data->price = calloc(data->price_size, sizeof(int));
     for (size_t i = 0; i < data->price_size; i++) {
-        if (fread(data->price + i, sizeof(float), 1, file) != 1) {
+        if (fread(data->price + i, sizeof(int), 1, file) != 1) {
             perror("Error reading price data");
             fclose(file);
             return 0;
@@ -194,7 +194,7 @@ int read_data(struct item_data *data) {
     return 1;
 }
 
-int add_data_to_struct(struct item_data *data, char name[STRING_LENGTH], char warehouse[STRING_LENGTH], char location[STRING_LENGTH], int quantity, float price) {
+int add_data_to_struct(struct item_data *data, char name[STRING_LENGTH], char warehouse[STRING_LENGTH], char location[STRING_LENGTH], int quantity, int price) {
 
     data->name_size += 1;
     data->warehouse_size += 1;
@@ -206,7 +206,7 @@ int add_data_to_struct(struct item_data *data, char name[STRING_LENGTH], char wa
     data->warehouse = realloc(data->warehouse, data->warehouse_size * STRING_LENGTH * sizeof(char));
     data->location = realloc(data->location, data->location_size * STRING_LENGTH * sizeof(char));
     data->quantity = realloc(data->quantity, data->quantity_size * sizeof(int));
-    data->price = realloc(data->price, data->price_size * sizeof(float));
+    data->price = realloc(data->price, data->price_size * sizeof(int));
 
     if (data->name == NULL || data->warehouse == NULL || data->location == NULL || data->quantity == NULL || data->price == NULL) {
         data->name_size -= 1;
@@ -226,19 +226,19 @@ int add_data_to_struct(struct item_data *data, char name[STRING_LENGTH], char wa
 }
 
 void delete_data_from_struct(struct item_data *data, int index) {
-    for (int i = index; i < data->name_size - 1; i++) {
+    for (int i = index; i < (int) data->name_size - 1; i++) {
         strcpy(data->name[i],  data->name[i + 1]);
     }
-    for (int i = index; i < data->warehouse_size - 1; i++) {
+    for (int i = index; i < (int) data->warehouse_size - 1; i++) {
         strcpy(data->warehouse[i],  data->warehouse[i + 1]);
     }
-    for (int i = index; i < data->location_size - 1; i++) {
+    for (int i = index; i < (int) data->location_size - 1; i++) {
         strcpy(data->location[i],  data->location[i + 1]);
     }
-    for (int i = index; i < data->quantity_size - 1; i++) {
+    for (int i = index; i < (int) data->quantity_size - 1; i++) {
         data->quantity[i] = data->quantity[i + 1];
     }
-    for (int i = index; i < data->price_size - 1; i++) {
+    for (int i = index; i < (int) data->price_size - 1; i++) {
         data->price[i] = data->price[i + 1];
     }
     data->name_size -= 1;
@@ -248,12 +248,93 @@ void delete_data_from_struct(struct item_data *data, int index) {
     data->price_size -= 1;
 }
 
-void update_data_in_struct(struct item_data *data, int index, char name[STRING_LENGTH], char warehouse[STRING_LENGTH], char location[STRING_LENGTH], int quantity, float price) {
+void update_data_in_struct(struct item_data *data, int index, char name[STRING_LENGTH], char warehouse[STRING_LENGTH], char location[STRING_LENGTH], int quantity, int price) {
     strcpy(data->name[index], name);
     strcpy(data->warehouse[index], warehouse);
     strcpy(data->location[index], location);
     data->quantity[index] = quantity;
     data->price[index] = price;
+}
+
+void print_padded_string(char *data) {
+    int length = (int) strlen(data) + 2;
+    int padding = ((STRING_LENGTH - length) / 2);
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+    printf("%s", data);
+    if (length + 2*padding < 20) {
+        padding += 1;
+    }
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+}
+
+int countDigits(int n) {
+    if (n == 0) return 1;
+    return (n / 10 == 0) ? 1 : 1 + countDigits(n / 10);
+}
+
+void print_padded_integer(int n) {
+    int length = countDigits(n) + 2;
+    int padding = (STRING_LENGTH - length) / 2;
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+    printf("%d", n);
+    if (length + 2*padding < 20) {
+        padding += 1;
+    }
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+}
+
+void list_struct(struct item_data *data) {
+
+    int table_width = 5 * (STRING_LENGTH + 2);
+
+    printf("|");
+    // table_width + 5 to include length of columns and -3 to remove already printed columns
+    for (int i = 0; i < (table_width + 3); i++) {
+        printf("-");
+    }
+    printf("|\n");
+
+    printf("|");
+    print_padded_string("INDEX");
+    printf("|");
+    print_padded_string("NAME");
+    printf("|");
+    print_padded_string("WAREHOUSE");
+    printf("|");
+    print_padded_string("LOCATION");
+    printf("|");
+    print_padded_string("QUANTITY");
+    printf("|");
+    print_padded_string("PRICE");
+    printf("|\n");
+    for (size_t i = 0; i < data->name_size; i++) {
+        printf("|");
+        print_padded_integer((int)(i + 1));
+        printf("|");
+        print_padded_string(data->name[i]);
+        printf("|");
+        print_padded_string(data->warehouse[i]);
+        printf("|");
+        print_padded_string(data->location[i]);
+        printf("|");
+        print_padded_integer(data->quantity[i]);
+        printf("|");
+        print_padded_integer(data->price[i]);
+        printf("|\n");
+    }
+    printf("|");
+    for (int i = 0; i < (table_width + 3); i++) {
+        printf("-");
+    }
+    printf("|\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -305,7 +386,7 @@ int main(int argc, char *argv[]) {
 // data.warehouse = malloc(count * STRING_LENGTH * sizeof(char));
 // data.location = malloc(count * STRING_LENGTH * sizeof(char));
 // data.quantity = malloc(count * sizeof(int));
-// data.price = malloc(count * sizeof(float));
+// data.price = malloc(count * sizeof(int));
 // if (!data.name) {
 //     perror("Failed to allocate memory");
 //     return 1;
@@ -339,7 +420,7 @@ int main(int argc, char *argv[]) {
 //
 // // Initialize quantity
 // for (int i = 0; i < 5; i++) {
-//     data.price[i] = (float)i;
+//     data.price[i] = (int)i;
 // }
 //
 // char name[20] = "Test name";
